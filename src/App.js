e9d548e98a4e19b93f127555d35661b59f2fd124
Paul Highum <phigh07@gmail.com>
2018-04-23 14:52:15 -0600

    setting up clean home screen

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">City Picker</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
