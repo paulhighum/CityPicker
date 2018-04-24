5e563e3a01d87ceaeee3bd943897c1b88d9175c6
Paul Highum <phigh07@gmail.com>
2018-04-24 15:11:40 -0600

    adds login component and btn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom"
-import './App.css';
+import './App.css'
+import Login from "./Login"
 
 class App extends Component {
-
-
 
   render() {
     return <Router>
@@ -13,14 +12,14 @@ class App extends Component {
             <h1 className="App-title">City Picker</h1>
           </header>
           <main>
-            {/* <Route exact path="/" render={() => <Redirect to="/main" />} />
-            <Route path="/login" render={() => <Login/>} />
-            <Route path="/profile" render={() => <Profile/>} />
+            {/* <Route exact path="/" render={() => <Redirect to="/main" />} /> */}
+            <Route path="/" render={() => <Login/>} />
+            {/* <Route path="/profile" render={() => <Profile/>} />
             <Route path="/main" render={() => <Main />} />
             <Route path="/logout" render={() => <Logout />} /> */}
           </main>
         </div>
-      </Router>;
+      </Router>
   }
 }
 
---
+++ src/Login/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import "./style.css"
+
+class Login extends Component {
+  render() {
+    return (
+      <div id="login-container">
+        <button id="login-btn"type="button">Log In</button>
+      </div>
+    )
+  }
+}
+
+export default Login
